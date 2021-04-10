54c2132631c9f1aee2d08287744dc3ac1c71d343
Josh_Oleary <jdoleary91@gmail.com>
2021-04-10 08:48:06 -0400

    first commit

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-var cityname = document.querySelector('#cityName');
-var temp = document.querySelector('#temperature');
-var btn = document.querySelector('#weatherBtn');
-var icon = document.querySelector('img');
-var cardContainer = document.querySelector('#cardContainer');
-var weatherDescription = document.querySelector('#weatherDescription');
-var input = document.querySelector('#inputForm');
-var errMsg = document.querySelector('#errorMsg');
+var input = document.querySelector('#input_form');
+var weatherCard = document.querySelector('#weather_card');
+var apiKey = '6a3a0b88d198abf6227eee7b2923106c';
 
-btn.addEventListener('click', function(e){
+
+input.addEventListener('submit', function(e){
     e.preventDefault();
-    let city = document.getElementById('city').value;
-    axios.get( `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=6a3a0b88d198abf6227eee7b2923106c`)
-        .then(function(response){
-            cardContainer.className = 'card';
-            let iconImg = response.data.weather[0].icon;
-            icon.src = `http://openweathermap.org/img/w/${iconImg}.png`
-            cityname.innerText = city;
-            weatherDescription.innerText = response.data.weather[0].description;
-            temp.innerHTML = Math.round(response.data.main.temp) + ' &#x2109';
-            input.reset();
-            errMsg.innerText = '';
-        })
-        .catch(function() {
-            if(errMsg.innerText === ''){
-                errMsg.innerText = "Please enter city name!"
-            } else {
-                errMsg.innerText = "City name invalid!";
-            }
-        })
+    let location = document.getElementById('location').value;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`)
+    .then(function(response){
+        let imgCode = response.data.weather[0].icon;
+        let newCard = document.createElement('div')
+        let cardHeading = document.createElement('h3');
+        let cardImg = document.createElement('img');
+        cardImg.src = `http://openweathermap.org/img/w/${imgCode}.png`;
+        let cardTemp = document.createElement('h4');
+        let cardDescription = document.createElement('p');
+        
+        
+        
+
+
+        cardHeading.innerText = response.data.name;
+        cardTemp.innerText = Math.floor(response.data.main.temp)
+        cardDescription.innerText = response.data.weather[0].description
+
+        newCard.className = 'card_container'
+        newCard.appendChild(cardHeading)
+        newCard.appendChild(cardImg);
+        newCard.appendChild(cardTemp)
+        newCard.appendChild(cardDescription);
+    
+        weatherCard.appendChild(newCard);
+        
+        document.getElementById('location').value = '';
+    })
 })
+
